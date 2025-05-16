d806079dce94d4d423a10b4ec7adc9c5b436ad43
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-05-16 19:32:10 +0900

    Health Check API 구현

---
+++ src/main/java/com/app/api/health/controller/HealthCheckController.java
@@ -0,0 +1,30 @@
+package com.app.api.health.controller;
+
+import com.app.api.health.dto.HealthCheckResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor   // 초기화되지 않은 필드, 초기화되지 않은 @NonNull로 표시된 필드를 자동 생성자 주입
+public class HealthCheckController {
+
+    // 현재 애플리케이션의 환경 설정 정보
+    private final Environment environment;
+
+    @GetMapping("/health")
+    public ResponseEntity<HealthCheckResponseDto> healthCheck() {
+        HealthCheckResponseDto healthCheckResponseDto = HealthCheckResponseDto.builder()
+                .health("ok")
+                .activeProfiles(Arrays.asList(environment.getActiveProfiles()))
+                .build();
+        // Edit configuration에서 Active profiles 추가 필요
+        return ResponseEntity.ok(healthCheckResponseDto);
+    }
+}
