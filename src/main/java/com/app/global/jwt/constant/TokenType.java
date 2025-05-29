b2acb51e25f8fbc2a197736a08b6ec8c4314eb19
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-05-29 09:47:47 +0900

    tokenType 확인 메소드 추가

--- src/main/java/com/app/global/jwt/constant/TokenType.java
+++ src/main/java/com/app/global/jwt/constant/TokenType.java
@@ -3,4 +3,10 @@ package com.app.global.jwt.constant;
 public enum TokenType {
 
     ACCESS, REFRESH;
+
+    public static boolean isAccessToken(String tokenType) {
+        // enum의 name() 메소드는 String 반환 (비슷한 enum의 메소드인 toString()처럼 재정의는 불가능)
+        return TokenType.ACCESS.name().equals(tokenType);
+    }
+
 }
