fbda43b984c4fc99d386ee543accdb5565b28085
hyolim922@hotmail.com <hyolim922@hotmail.com>
2025-06-02 12:43:18 +0900

    Member Service

---
+++ src/main/java/com/app/domain/member/service/MemberService.java
@@ -0,0 +1,57 @@
+package com.app.domain.member.service;
+
+import com.app.domain.member.entity.Member;
+import com.app.external.oauth.MemberRepository;
+import com.app.global.error.ErrorCode;
+import com.app.global.error.exception.AuthenticationException;
+import com.app.global.error.exception.BusinessException;
+import com.app.global.error.exception.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    // repository 계층는 service 계층을 통해서만 사용하면 유지보수가 수월
+    private final MemberRepository memberRepository;
+
+    public Member registerMember(Member member) {
+        validateDuplicateMember(member);
+        // 회원 테이블에 저장
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(member.getEmail());
+        if(optionalMember.isPresent()) {
+            throw new BusinessException(ErrorCode.ALREADY_REGISTERED_MEMBER);
+        }
+    }
+
+    public Optional<Member> findMemberByEmail(String email) {
+        return memberRepository.findByEmail(email);
+    }
+
+    // 영속성 컨텍스트에서의 변경 감지 기능 사용 X (읽기만) -> 성능 향상
+    @Transactional(readOnly = true)
+    public Member findMemberByRefreshToken(String refreshToken) {
+        Member member = memberRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new AuthenticationException(ErrorCode.REFRESH_TOKEN_NOT_FOUND));
+        LocalDateTime tokenExpirationTime = member.getTokenExpirationTime();
+        if(tokenExpirationTime.isBefore(LocalDateTime.now())) {
+            throw new AuthenticationException(ErrorCode.REFRESH_TOKEN_EXPIRED);
+        }
+        return member;
+    }
+
+    public Member findMemberByMemberId(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_EXISTS));
+    }
+}
